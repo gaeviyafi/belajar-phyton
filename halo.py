6ca34b0f9553afba15fc2e9e7e6dc2003872b9f3
gaeviyafi <111212795+gaeviyafi@users.noreply.github.com>
2022-09-11 15:04:09 +0700

    ini commit pertama

---
+++ halo.py
@@ -0,0 +1,2 @@
+#  ini adalah comment
+print("Hello, World!") 
---
+++ tipedata.py
@@ -0,0 +1,20 @@
+# ini tipe data string
+nama = "Budi"
+# ini tipe data integer
+umur = 17
+# ini tipe data float
+nilai_matematika = 7.5
+# ini tipe data array
+barang_bawaan = ["Sepatu", "Kaos", "Celana"]
+# ini tipe data Boolean
+Jawaban = True
+
+print("Halo nama saya ",nama)
+print("umur saya ",umur)
+print("barang bawaan saya",barang_bawaan)
+print("jawaban saya",Jawaban)
+
+print("tipe data nama adalah ",type(nama))
+print("tipe data umur adalah ",type(umur))
+print("tipe data barang bawaan adalah",type(barang_bawaan))
+print("tipe data jawaban adalah",type(Jawaban))
