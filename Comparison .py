4fa5c9693938dd23408fb107cb2c0603fdb763ee
gaeviyafi <111212795+gaeviyafi@users.noreply.github.com>
2022-10-09 14:26:48 +0700

    Operator,Logical,Comparasion

---
+++ Comparison .py
@@ -0,0 +1,11 @@
+a = int(input("Masukkan Bilangan Satu:"))
+b = int(input("Masukkan Bilangan Dua:"))
+
+# Arithmetic Operators 
+
+print(a < b)
+print(a <= b)
+print(a > b)
+print(a >= b)
+print(a == b)
+print(a != b)
---
+++ Operator.py
@@ -0,0 +1,15 @@
+a = int(input("Masukkan Bilangan Satu:"))
+b = int(input("Masukkan Bilangan Dua:"))
+
+# Arithmetic Operators 
+
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+print(f"a * b = {a * b}")
+print(f"a / b = {a / b}")
+#modulud sisa hasil bagi %
+print(f"a % b = {a % b}")
+print(f"a ** b = {a ** b}")
+print(f"a // b = {a // b}")
+ 
+ 
